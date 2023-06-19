493c68251e5088c804cd54796537583fbb51e138
lfcbivek <lfcbivek@gmail.com>
2023-06-18 21:38:30 -0400

    leaflet component added

--- flight_tracker_app/src/App.js
+++ flight_tracker_app/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
 
+import L from "leaflet";
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { Marker } from 'react-leaflet/Marker'
+import { Popup } from 'react-leaflet/Popup'
+import { useMap } from 'react-leaflet/hooks'
+
+const getIcon = () => {
+  console.log('here')
+  return L.icon({
+    iconUrl:'https://cdn-icons-png.flaticon.com/512/0/614.png',
+    iconSize: [50]
+  })
+}
 function App() {
+  const position = [51.505, -0.09];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker 
+          position={position}
+          icon={getIcon()}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
